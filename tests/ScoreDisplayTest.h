b48e6c1f7b59125e0637eb494041cd85e160ed93
Bliaxx <67436831+Bliaxx@users.noreply.github.com>
2024-06-14 22:07:56 +0200

    maj ScoreDisplayTest

--- tests/ScoreDisplayTest.h
+++ tests/ScoreDisplayTest.h
@@ -1,1 +1,44 @@
-#pragma once
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <SFML/Graphics.hpp>
+#include "ScoreDisplay.h"
+
+class MockScoreManager : public ScoreManager {
+public:
+    MOCK_METHOD(int, GetScore, (), (const, override));
+};
+
+class ScoreDisplayTest : public ::testing::Test {
+protected:
+    MockScoreManager mockScoreManager;
+    ScoreDisplay* scoreDisplay;
+    sf::RenderWindow window;
+
+    virtual void SetUp() {
+        scoreDisplay = new ScoreDisplay(mockScoreManager);
+    }
+
+    virtual void TearDown() {
+        delete scoreDisplay;
+    }
+};
+
+TEST_F(ScoreDisplayTest, InitialScoreText) {
+    EXPECT_CALL(mockScoreManager, GetScore()).WillOnce(testing::Return(0));
+    scoreDisplay->UpdateScoreText();
+    // Pas de méthode directe pour vérifier le texte, on vérifie donc si l'appel fonctionne sans erreur.
+}
+
+TEST_F(ScoreDisplayTest, UpdateScoreText) {
+    EXPECT_CALL(mockScoreManager, GetScore()).WillOnce(testing::Return(100));
+    scoreDisplay->UpdateScoreText();
+    // Ici aussi, on s'assure que l'appel fonctionne correctement.
+}
+
+TEST_F(ScoreDisplayTest, DrawScoreDisplay) {
+    EXPECT_CALL(mockScoreManager, GetScore()).WillOnce(testing::Return(200));
+    scoreDisplay->UpdateScoreText();
+    // Pour vérifier le rendu, nous nous assurons que Draw peut être appelé sans erreur.
+    scoreDisplay->Draw(window);
+}
+
